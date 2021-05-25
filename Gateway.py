8ddb9f26e9e25a36dee1625112b5c0e8e858a4f8
daze <leon.baiocchi@studio.unibo.it>
2021-05-25 18:18:34 +0200

    refactoring

--- Gateway.py
+++ Gateway.py
@@ -14,6 +14,10 @@ from threading import Thread
 
 class Gateway:
     def __init__(self, ip_UDP, port_UDP, ip_TCP, port_TCP):
+        # Attribute data_pool is a dictionary that contains pending
+        # fragments from iterations of manage_client() while True loop.
+        self.data_pool = {}
+        # Attribute clients contains clients that have connected to UDP server socket
         self.clients = {}
         self.filename = 'data_file.txt'
         # UDP Server socket setup
@@ -33,26 +37,38 @@ class Gateway:
         t = Thread(target=self.manage_client)
         t.start()
         t.join()
+        
     
-    def manage_client(self, data_pool):
+    def manage_client(self):
         # First receive helps to identify current client
         # Valid clients: the ones who send data
         first_data, curr_address = self.socket_UDP.recvfrom(4096)
         # Add current client address to the dictionary
         self.clients[curr_address[0]] = curr_address[1] 
+        # Every loop iteration refers to a single databox 
         while True:
             data, address = self.socket_UDP.recvfrom(4096)
             if not data:
+                self.pick_pending(curr_address)
                 break
             elif address != curr_address:
                 #TODO: add data_pool dictionary to implementation
+                self.data_pool[address] = data
                 continue
             else:
-                # Valid clients: the ones who send data
-                data = data.decode() # ???
-                print('received %s bytes from %s' % (len(data.encode()), address))
-                print('data:\n'+data)
+                self.print_received_data(data, address)
                 self.forward_data(data, address)
+    
+    def print_received_data(self, data, addr):
+        data = data.decode() # ???
+        print('received %s bytes from %s' % (len(data.encode()), addr))
+        print('data:\n'+data)
+                
+    def pick_pending(self, curr_addr):
+        for k in self.data_pool.keys():
+            if k == curr_addr:
+                self.print_received_data(self.data_pool[k]) 
+                self.forward_data(self.data_pool[k], curr_addr)
                     
     def forward_data(self, databox, dest_addr):
         ''' databox: 

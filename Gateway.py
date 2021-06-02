b5f5b29001d2b026187b30e72f49dbe42773ccbd
daze <leon.baiocchi@studio.unibo.it>
2021-06-02 17:50:24 +0200

    refactoring, pkt reception, arp table creation, decompose pkt, recompose pkt, send(not ultimated)

--- Gateway.py
+++ Gateway.py
@@ -9,23 +9,15 @@ Created on Mon May 10 16:37:59 2021
 from socket import AF_INET, socket, SOCK_DGRAM, SOCK_STREAM
 import sys, time
 import signal
-from threading import Thread
-
 
 class Gateway:
     def __init__(self, ip_port_UDP, ip_port_TCP, ip_devnet, ip_cloudnet, mac_addr, cloud_addr):
-        # ARP table creation
-        self.arp_table = {cloud_addr[0] : cloud_addr[1]}
-        # Set the gateway's 2 network interfaces' ips
+        # Set the gateway's 2 network interfaces' IPs
         self.ip_devnet = ip_devnet 
         self.ip_cloudnet = ip_cloudnet
         # Set gateway's mac address
         self.mac = mac_addr
-        # Attribute data_pool is a dictionary that contains pending
-        # fragments from iterations of manage_client() while True loop.
-        self.data_pool = {}
-        # Attribute clients contains clients that have connected to UDP server socket
-        self.clients = {}
+        #???
         self.filename = 'data_file.txt'
         # UDP Server socket setup
         self.socket_UDP = socket(AF_INET, SOCK_DGRAM)
@@ -39,64 +31,50 @@ class Gateway:
             print (Exception,":",data)
             sys.exit(0)
         print("TCP connection over Cloud established on port "+str(ip_port_TCP[1]))
-        
-    #Creates a thread that receives data
-    def get_file(self):
-        
-        t = Thread(target=self.manage_client)
-        t.start()
-        t.join()
+         # ARP table creation
+        self.arp_table_mac = {cloud_addr[0] : cloud_addr[1]}
+        self.arp_table_socket = {cloud_addr[0] : self.socket_TCP}
         
     def manage_client(self):
-        # First receive helps to identify current client
-        # Valid clients: the ones who send data
-        first_data, curr_address = self.socket_UDP.recvfrom(4096)
-        # Add current client address to the dictionary
-        self.clients[curr_address[0]] = curr_address[1] 
-        # Every loop iteration refers to a single databox 
         while True:
-            data, address = self.socket_UDP.recvfrom(4096)
-            if not data:
-                self.pick_pending(curr_address)
-                break
-            elif address != curr_address:
-                self.data_pool[address] = data
-                continue
-            else:
-                self.print_received_data(data, address)
-                self.forward_data(data, address)
-    
-    def print_received_data(self, data, addr):
-        data = data.decode() # ???
-        print('received %s bytes from %s' % (len(data.encode()), addr))
-        print('data:\n'+data)
-                
-    def pick_pending(self, curr_addr):
-        for k in self.data_pool.keys():
-            if k == curr_addr:
-                self.print_received_data(self.data_pool[k], k) 
-                self.forward_data(self.data_pool[k], k)
+            data, addr = self.socket_UDP.recvfrom(4096)
+            data = data.decode() #???
+            if data:
+                # Packet Headers and Content retrieval
+                source_ip = data[0:12]
+                destination_ip = data[12:22]
+                source_mac = data[22:39]
+                destination_mac = data[39:57]
+                message = data[57:]
+                # Important infos
+                print("The packed received:\n Source MAC address: {source_mac}, Destination MAC address: {destination_mac}".format(source_mac=source_mac, destination_mac=destination_mac))
+                print("\nSource IP address: {source_ip}, Destination IP address: {destination_ip}".format(source_ip=source_ip, destination_ip=destination_ip))
+                print("\nMessage: " + message)
+                # Packet Header recomposing
+                ethernet_header = self.mac + self.arp_table_mac[destination_ip]
+                IP_header = source_ip + destination_ip
+                headers = ethernet_header + IP_header
+                # Compose message and send
+                self.send_message(self.arp_table_socket[destination_ip], headers, 
+                                  source_ip, self.data_split(message))
+            time.sleep(.5)
                     
-    def forward_data(self, databox, dest_addr):
+    def data_split(self, databox):
         ''' databox: 
             ------------------------------------
-            \   <device-ip-address>            |
             |   time1 temperature1 humidity1   \
             \   time2 temperature2 humidity2   \
             |   ..... ............ .........   \
-            \   timeN temperatureN humidityN   \
+            \   timeN temperatureN humidityN   |
+            \                                  \ EOF
             ------------------------------------
         '''
-        databox = databox.decode()
         lines = databox.split("\n")
-        print("forwarding data to " + str(dest_addr))
-        ip = str(lines[0])
-        lines.remove(ip)
+        print("File split in lines..")
         lines.remove('') #EOF
-        print(str(lines))
-        self.send_message(ip, lines)
+        return lines
                 
-    def send_message(self, device_ip_addr, lines):
+    def send_message(self, dst_socket, headers, device_ip_addr, lines):
         message = ""
         previous = ""
         sent = False
@@ -110,13 +88,13 @@ class Gateway:
             else:
                 if previous != "":
                     message = message.replace(previous, "")
-                    self.socket_TCP.send(message.encode())
+                    self.dst_socket.send((headers+'\n'+message).encode())
                     sent = True
                     message = previous + current
             previous = current
         # Message never gets segmented
         if not sent:
-            self.socket_TCP.send(message.encode())
+            self.dst_socket.send((headers+'\n'+message).encode())
             
     def signal_handler(self, signal, frame):
         print('Ctrl+c pressed: sockets shutting down..')
@@ -132,5 +110,4 @@ if __name__ == '__main__':
                       ('10.10.10.2', 'FE:D7:0B:E6:43:C5'))
     signal.signal(signal.SIGINT, gateway.signal_handler)
     while True:
-        gateway.get_file()
-        time.sleep(.5)
+        gateway.manage_client()

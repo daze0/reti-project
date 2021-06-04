5fc2d495da48c8de5965c9edc05f9970a26c81ea
daze <leon.baiocchi@studio.unibo.it>
2021-06-04 19:28:01 +0200

    Renamed gateway module methods and instance variables following PEP 8 style guidelines

--- gateway.py
+++ gateway.py
@@ -13,40 +13,40 @@ import signal
 class Gateway:
     def __init__(self, ip_port_UDP, ip_port_TCP, ip_devnet, ip_cloudnet, mac_addr, cloud_addr):
         # Set the gateway's 2 network interfaces' IPs
-        self.ip_devnet = ip_devnet 
-        self.ip_cloudnet = ip_cloudnet
+        self._ip_devnet = ip_devnet 
+        self._ip_cloudnet = ip_cloudnet
         # Set gateway's mac address
-        self.mac = mac_addr
+        self._mac = mac_addr
         # UDP Server socket setup
-        self.socket_UDP = socket(AF_INET, SOCK_DGRAM)
-        self.socket_UDP.bind((ip_port_UDP))
+        self._socket_UDP = socket(AF_INET, SOCK_DGRAM)
+        self._socket_UDP.bind((ip_port_UDP))
         print("UDP server up on port "+str(ip_port_UDP[1]))
         # TCP Client socket setup
-        self.socket_TCP = socket(AF_INET, SOCK_STREAM)
+        self._socket_TCP = socket(AF_INET, SOCK_STREAM)
         try:
-            self.socket_TCP.connect((ip_port_TCP))
+            self._socket_TCP.connect((ip_port_TCP))
         except Exception as data:
             print (Exception,":",data)
             sys.exit(0)
         print("TCP connection over Cloud established on port "+str(ip_port_TCP[1]))
          # ARP table creation
-        self.arp_table_mac = {cloud_addr[0] : cloud_addr[1]}
-        self.arp_table_socket = {cloud_addr[0] : self.socket_TCP}
+        self._arp_table_mac = {cloud_addr[0] : cloud_addr[1]}
+        self._arp_table_socket = {cloud_addr[0] : self._socket_TCP}
         # Gateway main loop, CTRL+C signal handler
         signal.signal(signal.SIGINT, gateway.signal_handler)
-        self.manage_client()
+        self._manage_client()
         
-    def manage_client(self):
+    def _manage_client(self):
         while True:
-            data, addr = self.socket_UDP.recvfrom(4096)
+            data, addr = self._socket_UDP.recvfrom(4096)
             print("\nReceived {n_bytes} bytes..".format(n_bytes=len(data)))
             data = data.decode() 
             print("\n{data}".format(data=data))
             if data:
-                self.data_split(data)
+                self._data_split(data)
             time.sleep(.5)
                     
-    def data_split(self, databox):
+    def _data_split(self, databox):
         ''' databox: 
             ------------------------------------
             |   HEADERS                        |
@@ -78,15 +78,15 @@ class Gateway:
         print("\nEpoch time: {time},\nTime elapsed: {elapsed_time}".format(time=epoch_time, elapsed_time=time.time()-epoch_time))
         print("\nMessage: \n" + message)
         # Packet Header recomposing
-        ethernet_header = self.mac + self.arp_table_mac[destination_ip]
+        ethernet_header = self._mac + self._arp_table_mac[destination_ip]
         IP_header = source_ip + destination_ip
         epoch_time = time.time()
         new_headers = IP_header + ethernet_header + str(epoch_time)
         # Compose message and send
-        self.send_message(self.arp_table_socket[destination_ip], new_headers, 
+        self._send_message(self._arp_table_socket[destination_ip], new_headers, 
                           source_ip, lines)
                 
-    def send_message(self, dst_socket, headers, device_ip_addr, lines):
+    def _send_message(self, dst_socket, headers, device_ip_addr, lines):
         message = ""
         previous = ""
         sent = False
@@ -95,7 +95,7 @@ class Gateway:
                 line_data = line.split(" ")
                 current = device_ip_addr+" "+line_data[0]+" "+line_data[1]+" "+line_data[2]+"\n"
                 # Bulk up message with current new message part
-                if self.is_bulkable(headers+'\n'+message):
+                if self._is_bulkable(headers+'\n'+message):
                    message += current
                 # Segmentation 
                 else:
@@ -112,13 +112,13 @@ class Gateway:
         if not sent:
             dst_socket.send((headers+'\n'+message).encode())
             
-    def is_bulkable(self, msg):
+    def _is_bulkable(self, msg):
         return len(msg.encode()) < 4096
             
-    def signal_handler(self, signal, frame):
+    def _signal_handler(self, signal, frame):
         print('Ctrl+c pressed: sockets shutting down..')
         try:
-            self.socket_TCP.close()
+            self._socket_TCP.close()
         finally:
             sys.exit(0)
         

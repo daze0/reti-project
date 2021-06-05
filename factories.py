dc2aaac4ed4f7c079db27ade2437f7ac170eb077
daze <leon.baiocchi@studio.unibo.it>
2021-06-05 14:23:44 +0200

    magic numbers transformed to constants

--- factories.py
+++ factories.py
@@ -10,25 +10,28 @@ from device import Device
 from gateway import Gateway
 from cloud import Cloud
 
+DEFAULT_UDP_PORT = 10000
+DEFAULT_TCP_PORT = 45000
+
 class DeviceFactory:
     # Creates and returns an active customized device
     def custom_device(filename, device_ip, device_mac, gateway_addr, router_mac, target_ip):
         return Device(filename, device_ip, device_mac, gateway_addr, router_mac, target_ip)
     # Creates and returns default device 1
     def default_device_1():
-        return Device("data_dev1.txt", "192.168.1.10", "36:DF:28:FC:D1:67", ('localhost', 10000), 
+        return Device("data_dev1.txt", "192.168.1.10", "36:DF:28:FC:D1:67", ('localhost', DEFAULT_UDP_PORT), 
                   '7A:D8:DD:50:8B:42', '10.10.10.2')
     # Creates and returns default device 2
     def default_device_2():
-        return Device("data_dev2.txt", "192.168.1.15", "04:EA:56:E2:2D:63", ('localhost', 10000), 
+        return Device("data_dev2.txt", "192.168.1.15", "04:EA:56:E2:2D:63", ('localhost', DEFAULT_UDP_PORT), 
                   '7A:D8:DD:50:8B:42', '10.10.10.2')
     # Creates and returns default device 3
     def default_device_3():
-        return Device("data_dev3.txt", "192.168.1.20", "6A:6C:39:F0:66:7A", ('localhost', 10000), 
+        return Device("data_dev3.txt", "192.168.1.20", "6A:6C:39:F0:66:7A", ('localhost', DEFAULT_UDP_PORT), 
                   '7A:D8:DD:50:8B:42', '10.10.10.2')
     # Creates and returns default device 4
     def default_device_4():
-        return Device("data_dev4.txt", "192.168.1.25", "96:34:75:51:CC:73", ('localhost', 10000), 
+        return Device("data_dev4.txt", "192.168.1.25", "96:34:75:51:CC:73", ('localhost', DEFAULT_UDP_PORT), 
                   '7A:D8:DD:50:8B:42', '10.10.10.2')
     
 class GatewayFactory:
@@ -38,7 +41,7 @@ class GatewayFactory:
         return Gateway(ip_port_UDP, ip_port_TCP, ip_devnet, ip_cloudnet, mac_addr, cloud_addr)
     # Creates and returns default gateway
     def default_gateway():
-        return Gateway(('localhost', 10000), ('localhost', 45000), 
+        return Gateway(('localhost', DEFAULT_UDP_PORT), ('localhost', DEFAULT_TCP_PORT), 
                       '192.168.1.1', '10.10.10.1', '7A:D8:DD:50:8B:42',
                       ('10.10.10.2', 'FE:D7:0B:E6:43:C5'))
     
@@ -48,4 +51,4 @@ class CloudFactory:
         return Cloud(ip_n_port, ip, mac_addr)
     #Creates and returns default cloud server
     def default_cloud():
-        return Cloud(('localhost', 45000), '10.10.10.2', 'FE:D7:0B:E6:43:C5')
+        return Cloud(('localhost', DEFAULT_TCP_PORT), '10.10.10.2', 'FE:D7:0B:E6:43:C5')

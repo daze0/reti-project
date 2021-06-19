f919f92146bc05276221d8128e027dd9da1515c4
daze <leon.baiocchi@studio.unibo.it>
2021-06-19 13:04:09 +0200

    When user stops program subprocesses are now killed

--- test/test_devices.py
+++ test/test_devices.py
@@ -10,22 +10,38 @@ from subprocess import Popen
 import sys
 sys.path.append('../')
 import factories as f
+import signal
 
+def _signal_handler(self, signal, frame, subprocesses_list):
+        try:
+            for popen in subprocesses_list:
+                popen.kill()
+        finally:
+            print('Ctrl+c pressed: all subprocesses have been killed..')
+            sys.exit(0)
+            
 def execute(target):
-    Popen(["python", target + ".py"])
+    return Popen(["python", target + ".py"])
     
 def test():
     print("Device 1")
-    execute("test_dev1")
+    dev1 = execute("test_dev1")
     
     print("Device 2")
-    execute("test_dev2")
+    dev2 = execute("test_dev2")
     
     print("Device 3")
-    execute("test_dev3")
+    dev3 = execute("test_dev3")
     
     print("Device 4")
-    execute("test_dev4")
+    dev4 = execute("test_dev4")
+    
+    print("Press CTRL+C to kill each device execution")
+    # CTRL+C signal handler
+    signal.signal(signal.SIGINT, self._signal_handler, list(dev1, dev2, dev3, dev4))
+    running = True
+    while running:
+        continue
     
 if __name__ == "__main__":
     test()
